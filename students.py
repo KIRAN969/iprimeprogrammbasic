0f6a6ee10cfb9d7589e1be1b688a0dde7bf62003
KIRAN969 <rachapallekirankumarreddy@gmail.com>
2021-07-21 22:44:39 +0530

    set value

---
+++ students.py
@@ -0,0 +1,9 @@
+data=[{"names":"kiran","rollno":1710246,"adminno":12,"college":"skucet","marks":{"eng":41,"mat":45,"sci":46}},
+{"names":"charan","rollno":1710203,"adminno":22,"college":"skuceta","marks":{"eng":40,"mat":44,"sci":45}},
+{"names":"chinna","rollno":1710255,"adminno":15,"college":"skucetb","marks":{"eng":31,"mat":47,"sci":36}},
+{"names":"suri","rollno":1710238,"adminno":13,"college":"skucetc","marks":{"eng":41,"mat":45,"sci":46}},
+{"names":"reddy","rollno":1710227,"adminno":32,"college":"skucetd","marks":{"eng":41,"mat":45,"sci":46}}]
+print(data)
+# print(len(data))
+#print(data[0].keys())
+#print(data[0]["names"])
