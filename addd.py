9d6dfb662c34fa91a5dfcd1b4fc395b3a97c8acb
KIRAN969 <rachapallekirankumarreddy@gmail.com>
2021-07-23 11:37:14 +0530

    task

--- addd.py
+++ addd.py
@@ -4,3 +4,7 @@ n2=int(input("enter a number:"))
 # def addNum(a,b):
 #     return a+b
 print(addModule.addNum(n1,n2))
+print(addModule.subNum(n1,n2))
+print(addModule.mulNum(n1,n2))
+print(addModule.divNum(n1,n2))
+print(addModule.floorNum(n1,n2))
