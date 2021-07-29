7eb95aa5d43ce6a7b0112d2d712a2f8c1315c821
KIRAN969 <rachapallekirankumarreddy@gmail.com>
2021-07-29 13:02:39 +0530

    task

---
+++ day8/movie.py
@@ -0,0 +1,6 @@
+movie_names=["karthik","baahubali","sham","roy","killer"]
+new_list=[i for i in movie_names if 'a' in i ]
+# for i in movie_names:
+#     if 'a' in i:
+#         new_list.append(i)
+print(new_list)
